1a93467564e45bb53cb0c2ff63f22e0037e3d70c
Henry Goodman <13032204@student.uts.edu.au>
2022-08-24 20:46:28 +1000

    100 percent complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -6,15 +6,32 @@ public class PrintAndHide : MonoBehaviour
 {
     public Renderer rend;
     int i = 3;
+    int rand;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rand = Random.Range(200, 250);
     }
 
     // Update is called once per frame
     void Update()
     {
         Debug.Log(gameObject.name + ":" + ++i);
+        if (gameObject.tag.Equals("Red") && i == 100)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            gameObject.SetActive(true);
+        }
+        if (gameObject.tag.Equals("Blue") && i == rand)
+        {
+            rend.enabled = false;
+        } 
+        else
+        {
+            rend.enabled = true;
+        }
     }
 }
